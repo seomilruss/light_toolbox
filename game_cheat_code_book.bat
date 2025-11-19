0a87eb9ff736cea7fd580a2f76fab2e5e334f699
Cho Young Deok(조영덕) <54846678+seomilruss@users.noreply.github.com>
2025-11-19 14:58:11 +0900

    Add files via upload
    game_cheat_code_book(게임 치트코드 북) : 1. 설치 없이 가능(Can run without installation),
    2. 조작은 없지만 게임 치트키 정보를 보여줌(No manipulation, but show game cheat key information)
    3. 프리웨어(수정/배포) 허용[freeware (modification/deployment) allowed]

---
+++ game_cheat_code_book.bat
@@ -0,0 +1,26 @@
+@echo off
+REM 게임 치트키 보여주는 치트 코드북
+echo 스타크래프트 치트
+echo power overwhelming 무적
+echo show me the money (미네랄/가스)1만 증가
+echo black sheep wall 지도 밝히기
+echo food for thought 인구수 무제한
+echo operation cwal 모든 건설, 생산 속도 증가
+echo medieval man 스킬 업그레이드 ex)하이템플러, 건축하지 않아도 사이오닉에너지 기술 사용가능
+echo something for nothing 모든 업그레이드가 1 증가한다
+echo the gathering 마나 무한
+echo modify the phase variance 테크트리 무시
+echo war aint what it used to be 유닛이 지나간 자리 영구 시야 확보
+echo whats mine is mine 미네랄 자원 500
+echo breathe deep 가스 자원 500
+echo there is no cow level 강제 승리
+echo game over man 강제 패배
+echo staying alive 게임이 종료되지 않는다
+echo ophelia 미션 이동(입력후 종족 및 미션 번호 추가 입력)
+echo zoom zoom 화면 줌아웃(리마스터 전용)
+echo fart 적의 모든 가스를 0으로 만듬
+echo pickpocket 적 자원의 절반을 가져온다
+echo noglues 상대 마법금지
+echo radio free zerg 저그 배경음악 전환
+echo(
+pause
